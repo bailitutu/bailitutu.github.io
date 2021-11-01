bbad2b8c4e353bcf13669f703a332d31d0666d12
yefeng <yefeng.tzl@raycloud.com>
2021-11-01 12:05:06 +0800

    publish blog

---
+++ build-docs.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
+git init
+git add .
+git commit -m 'publish blog'
+
+# 部署到 https://bailitutu.github.io
+git push -u git@github.com:bailitutu/bailitutu.github.io.git blog
